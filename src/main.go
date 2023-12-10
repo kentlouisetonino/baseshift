241de17780798246291d5b0b8ac9c4a976d45e64
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-12-10 20:28:07 +0800

    format

--- src/main.go
+++ src/main.go
@@ -12,98 +12,97 @@ import (
 )
 
 func main() {
-  // Variable declations with initialization.
-  validOptions := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
-  isValidMainOption := false
-  hasError := false
-  
-  for {
-    // Clear up the screen first.
-    helpers.Clear()
+	// Variable declations with initialization.
+	validOptions := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
+	isValidMainOption := false
+	hasError := false
 
-    // Display the app description.
-    displays.AppDescription();
-    helpers.AddNewLine();
-    
-    // Display error message.
-    if (hasError) {
-      errors.InvalidOption();
-      helpers.AddNewLine()
-    }
+	for {
+		// Clear up the screen first.
+		helpers.Clear()
 
-    // Display the options.
-    displays.AppOptions();
-    helpers.AddNewLine();
+		// Display the app description.
+		displays.AppDescription()
+		helpers.AddNewLine()
 
-    // Ask input.
-    userInput := inputs.MainOptionInput()
-    
-    // Exit the application.
-    if (userInput == 13) {
-      break
-    }
-    
-    // Check if the option is valid.
-    isValidMainOption = slices.Contains(validOptions, userInput)
-    
-    if isValidMainOption {
-      helpers.Clear()
-      hasError = false;
-      
-      if (userInput == 1) {
-        // This will handle the loop if option1 input is invalid.
-        option1Menu := 0
-        option1Input := int64(0)
-        option1HasError := false
-        backToMainMenu := false
-        
-        for backToMainMenu == false {
-          // Clear first the terminal.
-          helpers.Clear()
+		// Display error message.
+		if hasError {
+			errors.InvalidOption()
+			helpers.AddNewLine()
+		}
 
-          // Display option 1 description.
-          displays.Option1Description()
-          helpers.AddNewLine()
+		// Display the options.
+		displays.AppOptions()
+		helpers.AddNewLine()
 
-          // Display error message.
-          if (option1HasError) {
-            errors.InvalidOption();
-            helpers.AddNewLine()
-          }
+		// Ask input.
+		userInput := inputs.MainOptionInput()
 
-          // Ask binary input.
-          option1Input = inputs.Option1Input()
-            
-          if (option1Input == -1) {
-            fmt.Println(option1Input)
-            // Signal the app that there is an error.
-            option1HasError = true
+		// Exit the application.
+		if userInput == 13 {
+			break
+		}
 
-            continue
-          } else {
-            // Display the output of computation ffor option 1.
-            fmt.Println(helpers.TwoSpace, "Decimal :", computations.BinaryToDecimal(option1Input))
-            helpers.AddNewLine()
+		// Check if the option is valid.
+		isValidMainOption = slices.Contains(validOptions, userInput)
 
-            // Ask user if want to try again, go back, or quit.
-            // Any other key except 1 and 2 will be treated as quit.
-            fmt.Print(helpers.TwoSpace, helpers.ColorGreen, " [1-Retry, 2-Back] : ", helpers.ColorReset)
-            fmt.Scan(&option1Menu)
-            
-            if (option1Menu == 1) {
-              option1HasError = false
-              continue
-            } else if (option1Menu == 2) {
-              backToMainMenu = true
-            } else {
-              break
-            }
-          }
-        }
-      }
-    } else {
-      hasError = true;
-    }
-  }
+		if isValidMainOption {
+			helpers.Clear()
+			hasError = false
+
+			if userInput == 1 {
+				// This will handle the loop if option1 input is invalid.
+				option1Menu := 0
+				option1Input := int64(0)
+				option1HasError := false
+				backToMainMenu := false
+
+				for backToMainMenu == false {
+					// Clear first the terminal.
+					helpers.Clear()
+
+					// Display option 1 description.
+					displays.Option1Description()
+					helpers.AddNewLine()
+
+					// Display error message.
+					if option1HasError {
+						errors.InvalidOption()
+						helpers.AddNewLine()
+					}
+
+					// Ask binary input.
+					option1Input = inputs.Option1Input()
+
+					if option1Input == -1 {
+						fmt.Println(option1Input)
+						// Signal the app that there is an error.
+						option1HasError = true
+
+						continue
+					} else {
+						// Display the output of computation ffor option 1.
+						fmt.Println(helpers.TwoSpace, "Decimal :", computations.BinaryToDecimal(option1Input))
+						helpers.AddNewLine()
+
+						// Ask user if want to try again, go back, or quit.
+						// Any other key except 1 and 2 will be treated as quit.
+						fmt.Print(helpers.TwoSpace, helpers.ColorGreen, " [1-Retry, 2-Back] : ", helpers.ColorReset)
+						fmt.Scan(&option1Menu)
+
+						if option1Menu == 1 {
+							option1HasError = false
+							continue
+						} else if option1Menu == 2 {
+							backToMainMenu = true
+						} else {
+							break
+						}
+					}
+				}
+			}
+		} else {
+			hasError = true
+		}
+	}
 }
-
