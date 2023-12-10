33a0a7ed4375dd610237bc999015d5c653c882d1
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-12-10 20:30:56 +0800

    format again

--- src/main.go
+++ src/main.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 	// Variable declations with initialization.
-	validOptions := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
+  validOptions := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
 	isValidMainOption := false
 	hasError := false
 
