1d9e50e9f2c37a6640d48af6e6d7ca715f61d02d
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-12-29 20:22:50 +0800

    update: make it work

--- src/services/binary_service.go
+++ src/services/binary_service.go
@@ -44,7 +44,7 @@ func getBinaryInput() int64 {
 	var optionChecker string
 
 	// Ask the option.
-	fmt.Print(helpers.ThreeSpace, "Binary  : ")
+	fmt.Print(helpers.ThreeSpace, "Binary", helpers.ThreeSpace, helpers.ThreeSpace, ": ")
 
 	// Check if the string is a valid number.
 	fmt.Scan(&optionChecker)
@@ -105,7 +105,7 @@ func getOctalValue(binary int64) string {
 		isLastIndex := i == (len(reversedBinaryArray) - 1)
 
 		// If remainder is 0, weight is 1.
-		if numWeight%3 == 0 {
+		if numWeight == 0 {
 			groupSum += reversedBinaryArray[i] * 1
 
 			// Push the value right away, if it is the last index.
@@ -115,7 +115,7 @@ func getOctalValue(binary int64) string {
 		}
 
 		// If remainder is 1, weight is 2.
-		if numWeight%3 == 1 {
+		if numWeight == 1 {
 			groupSum += reversedBinaryArray[i] * 2
 
 			// Push the value right away, if it is the last index.
@@ -125,7 +125,7 @@ func getOctalValue(binary int64) string {
 		}
 
 		// If remainder is 2, weight is 4.
-		if numWeight%3 == 2 {
+		if numWeight == 2 {
 			arrayOfOctal = append(arrayOfOctal, groupSum+(reversedBinaryArray[i]*4))
 			groupSum = 0
 		}
@@ -140,6 +140,87 @@ func getOctalValue(binary int64) string {
 	}
 
 	return octal
+}
+
+// Converts binary number to hexadecial.
+func getHexadecimalValue(binary int64) string {
+	hexadecimal := ""
+	reversedBinaryArray := getReversedBinaryArray(binary)
+	arrayOfHexadecimal := []int64{}
+	groupSum := int64(0)
+
+	for i := 0; i < len(reversedBinaryArray); i++ {
+		numWeight := i % 4
+		isLastIndex := i == (len(reversedBinaryArray) - 1)
+
+		// If remainder is 0, weight is 1.
+		if numWeight == 0 {
+			groupSum += reversedBinaryArray[i] * 1
+
+			// Push the value right away, if it is the last index.
+			if isLastIndex {
+				arrayOfHexadecimal = append(arrayOfHexadecimal, groupSum)
+			}
+		}
+
+		// If remainder is 1, weight is 2.
+		if numWeight == 1 {
+			groupSum += reversedBinaryArray[i] * 2
+
+			// Push the value right away, if it is the last index.
+			if isLastIndex {
+				arrayOfHexadecimal = append(arrayOfHexadecimal, groupSum)
+			}
+		}
+
+		// If remainder is 2, weight is 4.
+		if numWeight == 2 {
+			groupSum += reversedBinaryArray[i] * 4
+
+			// Push the value right away, if it is the last index.
+			if isLastIndex {
+				arrayOfHexadecimal = append(arrayOfHexadecimal, groupSum)
+			}
+		}
+
+		if numWeight == 3 {
+			arrayOfHexadecimal = append(arrayOfHexadecimal, groupSum+(reversedBinaryArray[i]*8))
+			groupSum = 0
+		}
+	}
+
+	// Reversed the hexadecimal array first before processing it.
+	reversedArrayOfHexadecimal := getReverseIntArray(arrayOfHexadecimal)
+
+	// Loop the array of hexadecimal.
+	for i := 0; i < len(reversedArrayOfHexadecimal); i++ {
+		hex := strconv.FormatInt(int64(reversedArrayOfHexadecimal[i]), 10)
+
+		switch hex {
+		case "10":
+			hexadecimal += "A"
+			break
+		case "11":
+			hexadecimal += "B"
+			break
+		case "12":
+			hexadecimal += "C"
+			break
+		case "13":
+			hexadecimal += "D"
+			break
+		case "14":
+			hexadecimal += "E"
+			break
+		case "15":
+			hexadecimal += "F"
+			break
+		default:
+			hexadecimal += hex
+		}
+	}
+
+	return hexadecimal
 }
 
 func BinaryToDecimal() {
@@ -173,7 +254,7 @@ func BinaryToDecimal() {
 			continue
 		} else {
 			// Display the output of computation for option 1.
-			fmt.Println(helpers.TwoSpace, "Decimal :", getDecimalValue(optionInput))
+			fmt.Println(helpers.TwoSpace, "Decimal", helpers.ThreeSpace, ": ", getDecimalValue(optionInput))
 			helpers.AddNewLine()
 
 			// Ask user if want to try again, go back, or quit.
@@ -225,7 +306,7 @@ func BinaryToOctal() {
 			continue
 		} else {
 			// Display the output of computation for option 2.
-			fmt.Println(helpers.TwoSpace, "Octal", helpers.OneSpace, ":", getOctalValue(optionInput))
+			fmt.Println(helpers.TwoSpace, "Octal", helpers.ThreeSpace, helpers.OneSpace, ": ", getOctalValue(optionInput))
 			helpers.AddNewLine()
 
 			// Ask user if want to try again, go back, or quit.
@@ -277,7 +358,7 @@ func BinaryToHexadecimal() {
 			continue
 		} else {
 			// Display the output of computation for option 2.
-			fmt.Println(helpers.TwoSpace, "Hexadecimal", helpers.OneSpace, ":", getOctalValue(optionInput))
+			fmt.Println(helpers.TwoSpace, "Hexadecimal :", getHexadecimalValue(optionInput))
 			helpers.AddNewLine()
 
 			// Ask user if want to try again, go back, or quit.
